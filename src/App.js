4a28591d52ad25c3bb1b66e82342d83558d96f3e
Mathis <mathis.louarn@etu.univ-amu.fr>
2024-03-17 15:29:27 +0100

    Premier commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="completion">
+
+        </header>
+        <div className="taskList">
+          <p>test? insane??</p>
+        </div>
+          <footer className="searchBar">
+
+          </footer>
+      </div>
   );
 }
 
